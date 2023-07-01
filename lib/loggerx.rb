82a6471891e6c85b56bdc1818ec9114e2a85635e
ykominami <ykominami@gmail.com>
2023-07-01 15:52:50 +0900

    initial commit

---
+++ lib/loggerx.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "loggerx/version"
+require_relative "loggerx/loggerx"
+
+module Loggerx
+  class Error < StandardError; end
+  # Your code goes here...
+end
